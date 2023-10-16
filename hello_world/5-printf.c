6dab17837c6d9eba00203e3c9da6d676cc570d2a
CreatorCosmo <girizarrymartell14@gmail.com>
2023-10-16 12:00:40 -0400

    ts

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 /**
-* main - Entry Point
-* Description - 'printing a specified message with printf'
-* Return: Always 0 (Sucess)
+ * main-displays printf message
+ * Return: 0 (Sucess)
 */
+
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
 }
