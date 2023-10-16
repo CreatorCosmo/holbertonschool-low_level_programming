68c08e7e7d191870deed5653d13f0985edda9d10
CreatorCosmo <girizarrymartell14@gmail.com>
2023-10-16 11:28:52 -0400

    ts

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -4,10 +4,13 @@
  *
  * Description 'printing a specific message with printf'
  *
+ * main - 'description of main'
  * Return: Always 0 (Sucess)
  */
-PrintMessage(void)
+void PrintMessage(void)
 {
-printf("with proper grammar, but the outcome is a piece of art")
+printf("with proper -grammar, but the outcome is a piece of art")
+void main(void)
 return (0);
+PrintMessage(void);
 }
