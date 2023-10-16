f5502eda31ab2032e46f769e073fd04abdcc8e8e
CreatorCosmo <girizarrymartell14@gmail.com>
2023-10-16 11:21:57 -0400

    write a C program that prints a message with printf

---
+++ hello_world/5-prinf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * PrintMessage - Entry point
+ *
+ * Description 'printing a specific message with printf'
+ *
+ * Return: Always 0 (Sucess)
+ */
+PrintMessage(void)
+{
+printf("with proper grammar, but the outcome is a piece of art")
+return (0);
+}
