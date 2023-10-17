9b2c3d67496d94fd3ec52aa45f7c8b35fb3f5b92
CreatorCosmo <girizarrymartell14@gmail.com>
2023-10-17 13:48:36 -0400

    cleaned code to abide to betty coding style

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,10 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/*
-*main - Entry point
-*Description: 'prints the last digit with extra specifications'
-*Return: Always (0) 
+/**
+* main - Entry point
+* Description: 'prints the last digit with extra specifications'
+* Return: Always (0)
 */
 
 int main(void)
@@ -26,7 +26,7 @@ if ((n % 10) > 5)
 	printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
 }
 
-else if ((n % 10) < 6 && (n % 10) != 0) 
+else if ((n % 10) < 6 && (n % 10) != 0)
 {
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 }
