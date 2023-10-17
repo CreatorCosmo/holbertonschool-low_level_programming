ca37d23f2ae982784a37f89fce2593006074235e
CreatorCosmo <girizarrymartell14@gmail.com>
2023-10-17 14:42:24 -0400

    cleaned code, added comments and made some changes in order to abide to Betty coding style

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
+/**
+* main - Entry Point
+* Description: 'printing alphabet in lowercase, then uppercase"
+* Return: Always 0 (Sucess)
+*/
 
 int main(void)
 {
 char i;
 
 
-for (i ='a'; i <= 'z'; i++)
-	printf("%c ", i);
+for (i = 'a'; i <= 'z'; i++)
+	printf("%c", i);
 
-for (i ='A'; i <='Z'; i++)
-printf("%c ", i);
+for (i = 'A'; i <= 'Z'; i++)
+printf("%c", i);
 
-return 0;
+return (0);
 }
