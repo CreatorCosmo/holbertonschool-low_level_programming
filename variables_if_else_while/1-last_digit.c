8e3e217cdfe990623e27727d5444e947350e29ee
CreatorCosmo <girizarrymartell14@gmail.com>
2023-10-17 10:54:11 -0400

    The output of the program should be: The string Last digit of, followed by n, followed by the string is, followed by if the last digit of n is greater than 5: the string and is greater than 5 if the last digit of n is 0: the string and is 0 if the last digit of n is less than 6 and not 0: the string and is less than 6 and not 0 followed by a new line

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n, last_digit;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	
+	last_digit = n % 10;
+
+/* Displaying output */
+
+printf("Last digit of %d is: %d", n, last_digit);
+
+/* if statements */
+
+
+if (n > 5)
+{
+printf("and is greater than 5");
+}
+
+
+if (n == 0)
+{
+printf("and is 0");
+}
+
+
+if (n < 6)
+{
+printf("and is less than 6 and not 0");
+}
+	return (0);
+}
