514c6a4ec9c95e324c3c79e2cbe6cb3f25c721e8
CreatorCosmo <girizarrymartell14@gmail.com>
2023-10-17 14:05:37 -0400

    ts

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/**
+* main - Entry Point
+* Description: 'print alphabet in lowercase followed by a new line'
+* Return: Always 0 (Sucess)
+*/
 int main(void)
 {
 char alphabet;
