f8a10c9c4fc371deeb1614ec770576d91c2f3a5c
CreatorCosmo <girizarrymartell14@gmail.com>
2023-10-17 11:19:02 -0400

    ts

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -27,13 +27,6 @@ if (n > 5)
 printf(" and is greater than 5\n");
 }
 
-
-else
-
-	printf(" and is 0\n");
-
-
-
 if (n < 6)
 {
 printf(" and is less than 6 and not 0\n");
@@ -42,7 +35,6 @@ printf(" and is less than 6 and not 0\n");
 else
 {
 	printf("  and is 0\n");
-}
 
 	return (0);
 }
