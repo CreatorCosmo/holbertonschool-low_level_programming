a6722a06acb7a9510c083d98ba1fe9e464bdbe86
CreatorCosmo <girizarrymartell14@gmail.com>
2023-10-17 10:58:23 -0400

    ts

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -22,19 +22,19 @@ printf("Last digit of %d is: %d", n, last_digit);
 
 if (n > 5)
 {
-printf(" and is greater than 5");
+printf(" and is greater than 5\n");
 }
 
 
 if (n == 0)
 {
-printf(" and is 0");
+printf(" and is 0\n");
 }
 
 
 if (n < 6)
 {
-printf(" and is less than 6 and not 0");
+printf(" and is less than 6 and not 0\n");
 }
 	return (0);
 }
