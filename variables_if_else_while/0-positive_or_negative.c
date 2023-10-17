d1aa67a8c9409402e251701806dd63936f347954
CreatorCosmo <girizarrymartell14@gmail.com>
2023-10-17 09:49:52 -0400

    fixed the position of the \n to insert a new line

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -13,16 +13,16 @@ int main(void)
 
 	/* if statement if n is greater than 0 is positive */
 	if (n > 0) {
-		printf("%d is positive", n \n);
+		printf("%d is positive\n", n);
 	}
 
 	/*if statement if n is equal to 0 is zero */
 	if (n == 0) {
-		printf("%d is zero", n);
+		printf("%d is zero\n", n);
 	}
 	/* if statement is less than 0 is negative */
 	if (n < 0) {
-		printf("%d is negative", n);
+		printf("%d is negative\n", n);
 	}
 	return (0);
 }
