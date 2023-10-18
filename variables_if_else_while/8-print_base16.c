22b7047de5bf0c8b1bca6013481aa1668b57a527
CreatorCosmo <girizarrymartell14@gmail.com>
2023-10-18 10:49:15 -0400

    Write a program that prints all the numbers of base 16 in lowercase, followed by a new line.

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - prints numbers of base 16 in lowercase
+* Description: 'print all numbers of base 16 in lowercase'
+* Return: 0
+*/
+
+int main(void)
+{
+    int num;
+
+    for (num = 0; num <= 15; num++)
+    {
+        printf("%x", num);
+    }
+    printf("\n");
+    return (0);
+}
