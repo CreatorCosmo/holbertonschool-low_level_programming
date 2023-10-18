69f950591133e2307b8945115eab785a5efc028d
CreatorCosmo <girizarrymartell14@gmail.com>
2023-10-18 10:06:23 -0400

    ts

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
- /**
+/**
 * main - Entry Point
-* Description: 'printing alphabet in lowercase, except letter e nor q"
+* Description: 'printing alphabet in lowercase, except letter e nor q'
 * Return: Always 0 (Sucess)
 */
- 
+
 int main(void)
 
 {
 char i;
- 
+
 for (i = 'a'; i <= 'z'; i++)
 {
 if (i != 'e' && i != 'q')
