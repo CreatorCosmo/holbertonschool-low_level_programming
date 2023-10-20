53aa3d37631d1f7b7f8099148ef3a3f7444c6f93
CreatorCosmo <girizarrymartell14@gmail.com>
2023-10-20 10:27:12 -0400

    added Return description

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -4,6 +4,7 @@
 * add - Adds two intergers and returns the result
 * @n1: The first number to be computed
 * @n2: The second number to be computed
+* Return: Result of the computed interger
 */
 
 int add(int n1, int n2)
