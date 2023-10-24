f88129b03f73f959792fb9fa272929631c2ecd39
CreatorCosmo <girizarrymartell14@gmail.com>
2023-10-24 11:31:43 -0400

    changed code structure

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -2,8 +2,9 @@
 #include <stdio.h>
 
 /**
-* _strlen - Returns the length of a specified string.
+* _strlen - Returns the length of a specified string
 * @s: Specified string to ge the length value
+* Return: Return the length
 */
 
 int _strlen(char *s)
