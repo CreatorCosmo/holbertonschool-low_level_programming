56f6e5d3b413a5da87eef7fee9a26917be8cd4f0
CreatorCosmo <girizarrymartell14@gmail.com>
2023-10-26 08:57:17 -0400

    changes

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -5,13 +5,17 @@
 *	including the terminating null byte (\0), to the buffer pointed to by dest
 * @dest: Destination to copy the string
 * @src: The source string copy
+* Return: The pointer to dest
 */
 
 char *_strcpy(char *dest, char *src)
+
 {
 	int i;
-	for (i = 0; src[i] != '\0'; i++)
+
+for (i = 0; src[i] != '\0'; i++)
 {
+
 dest[i] = src[i];
 }
 dest[i++] = '\0';
