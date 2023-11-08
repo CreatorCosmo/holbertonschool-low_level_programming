8512a67ea76ce0fcde013b05c12d50dc0f52fb64
CreatorCosmo <girizarrymartell14@gmail.com>
2023-11-07 23:29:34 -0400

    compiled .c

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -2,18 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 /**
- *free_grid- fuction to free incoming array from malloc
+ * free_grid- fuction to free incoming array from malloc
  *
- *@grid: ptr of array 2d from malloc
- *@height: size of dimension 2
+ * @grid: ptr of array 2d from malloc
+ * @height: size of dimension 2
  */
 void free_grid(int **grid, int height)
 {
 	int i;
 
 	for (i = 0; i < height; i++)
-	{	
-free(grid[i]);
-	}
-	free(grid[i]);
+		free(grid[i]);
+	free(grid);
 }
