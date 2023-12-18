6946d5797225a3579b36e7572dd0c4d1c5d43d27
CreatorCosmo <girizarrymartell14@gmail.com>
2023-12-17 23:27:15 -0400

    added changes

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "hash_tables.h"
+/**
+ * hash_table_create- function that creates a hash table
+ *
+ * @size: size of the array
+ *
+ * Return: pointer to hash table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int i;
+	hash_table_t *hash_table;
+
+
+
+	hash_table = malloc(sizeof(hash_table_t)); /* make hash table */
+	if (hash_table == NULL)
+		return (NULL);
+
+	/*
+	 * next line assigns malloc for each first node
+	 * our array index points to the first node of each list
+	 */
+
+	hash_table->array = malloc(sizeof(hash_node_t *) * size);
+
+	if (hash_table->array == NULL)
+	{
+		free(hash_table);
+		return (NULL);
+	}
+
+	hash_table->size = size;/* size of the array for hash table */
+	for (i = 0; i < size; i++) /* initialize nodes */
+		hash_table->array[i] = NULL;
+
+	return (hash_table);
+}
